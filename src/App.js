3a41044170f374d534782e7871e50bfc2e9aeeaa
ManoharGajangi-1037 <manohargajangi1037@gmail.com>
2024-04-08 15:55:40 +0530

    latest

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import logo from './logo.svg';
 import './App.css';
 import Navbar from './Navbar';
 import IndexPage from './IndexPage';
-import { BrowserRouter ,Route,Routes, Switch } from 'react-router-dom';
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import DealsComponent from './DealsComponent';
 import CreateDeal from './CreateDeal';
 import Profile from './Profile';
@@ -12,30 +12,10 @@ import Bid from './Bid';
 function App() {
   return (
     <div className="App">
-      {/* <Router>
-        <Switch>
-          <Route exact path="/">
-            <IndexPage />
-          </Route>
-          <Route exact path="/deals">
-            <DealsComponent />
-          </Route>
-          <Route exact path="/create-deal">
-            <CreateDeal></CreateDeal>
-          </Route>
-          <Route exact path="/profile">
-            <Profile></Profile>
-          </Route>
-          <Route exact path="/bid">
-            <Bid></Bid>
-          </Route>
-        </Switch>
-      </Router> */}
-
-<BrowserRouter>
+          <BrowserRouter>
       <Routes>
-        <Route path="/" element={<IndexPage/>}>
-          <Route path="deals" element={<DealsComponent />} />  
+        <Route path="/" element={<IndexPage />}>
+        <Route path='/deals'  element={<DealsComponent />}></Route>
         </Route>
       </Routes>
     </BrowserRouter>
--- src/CreateDeal.js
+++ src/CreateDeal.js
@@ -1,4 +1,4 @@
-import { Link } from "react-router-dom/cjs/react-router-dom.min"
+
 const CreateDeal = () => {
     return ( 
         <>
